bbb78b68903ad02c054a7797e0bb137920144d7a
Saurav <61650295+SauravRana19@users.noreply.github.com>
2023-02-17 16:11:03 +0530

    JavaScript Projects Small

---
+++ javascript-projects-main/Calculator/javascript.js
@@ -0,0 +1,17 @@
+let display = document.getElementById("screen");
+function allclear() {
+  display.value = "";
+}
+
+function clear() {
+  display.value = display.value.slice(-1);
+}
+
+function show(n) {
+  display.value += n;
+}
+
+function calc() {
+  display.value = eval(display.value);
+}
+
